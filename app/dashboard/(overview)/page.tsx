d4e7bd0aa265e1b7757495e6450c6bf150cd3f4b
andreluizsd <andre.ribeiro@engenharia.ufjf.br>
2023-11-02 20:35:09 -0300

    test table

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
